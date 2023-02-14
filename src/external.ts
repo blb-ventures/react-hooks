d8bd41e105ae34348d34131a5d0cac80d98cfff9
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2023-02-14 18:46:21 -0300

    fix: adds missing export for the use reisze observer hook

--- src/external.ts
+++ src/external.ts
@@ -2,4 +2,5 @@ export * from './use-dialogs';
 export * from './use-dynamic-load';
 export * from './use-event';
 export * from './use-polling';
+export * from './use-resize-observer';
 export * from './use-window-size';
